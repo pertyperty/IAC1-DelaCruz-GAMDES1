087cda3c2afd31cd0f8d5ce764a6fe290d45322c
pertypert <20244042@s.ubaguio.edu>
2026-08-25 12:51:37 +0800

    Added Lab1 activity

--- fg-08-25-2026-demo-project/label.gd
+++ fg-08-25-2026-demo-project/label.gd
@@ -2,7 +2,7 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print()
+	print("Miss ka ba niya?")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
